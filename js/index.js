4118400e3d119cbb06eb272fee38fc1220f1ac13
corplink <malwarecorp0@gmail.com>
2025-05-12 18:41:45 +0600

    Delete js directory

--- js/index.js
+++
@@ -1,5 +0,0 @@
-// index.js - entry point for modular JavaScript
-import { initApp } from './modules/main.js';
-
-// Initialize application when DOM is loaded
-document.addEventListener('DOMContentLoaded', initApp);
